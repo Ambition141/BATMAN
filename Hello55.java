431f70d4911a704ef41d29965474f1d50c883d4e
Ambition141 <61616099+Ambition141@users.noreply.github.com>
2020-03-03 09:22:04 +0800

    Add files via upload

---
+++ Hello55.java
@@ -0,0 +1,19 @@
+package hello55;
+
+import java.util.Scanner;
+
+public class Hello55 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("ÇëÍ¶±Ò");
+//		Scanner in=new Scanner(System.in);
+//		int amount;
+//		amount=in.nextInt();
+//		System.out.println(amount>=10);
+		double a=1.0;
+		double b=0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1;
+		System.out.println(a==b);
+	}
+
+}
